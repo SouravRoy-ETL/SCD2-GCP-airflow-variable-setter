3751d7eaae74b259c06777c6501ed9580899d4fe
Sourav Roy <85476817+SouravRoy-ETL@users.noreply.github.com>
2021-06-22 18:55:04 +0530

    Add files via upload

---
+++ input/YML/remove_last_stage_table.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import shutil
+import time
+from pathlib import Path
+with open('NC25_SCD2_config_1624360292.yml', 'r') as file :
+  filedata = file.read()
+filedata = filedata.replace("'bq.stage.table'","'bq.stage_table'")
+with open('NC25_SCD2_config_1624360292.yml', 'w') as file:
+  file.write(filedata)
